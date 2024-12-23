70cc30b372836f8ff55e1ffdfdd8fa9d1855a71e
asiisii <ashishmalla45@gmail.com>
2024-12-22 19:30:54 -0700

    controllers and actions wrap up

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  get "/products", to: "products#index"
+  # get "/products", to: "products#index"
 
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
 
-  get "/products/:id", to: "products#show"
+  # get "/products/:id", to: "products#show"
 
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
 
-  delete "/products/:id", to: "products#destroy"
+  # delete "/products/:id", to: "products#destroy"
 
   # above routes can be replaced with resources
-  # resources :products
+  resources :products
 
 
   # Defines the root path route ("/")
